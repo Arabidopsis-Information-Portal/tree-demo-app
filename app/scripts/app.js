c7a45a8d3aaea45cdec05c98ad0bf94daf1dfc44
eriksf <erik@jcvi.org>
2016-03-08 18:07:09 -0500

    changed draw tool from jsPhyloSVG to BioJS tnt.tree

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,35 +1,72 @@
 /*jshint camelcase: false*/
 /*jshint quotmark: true*/
-/* global Smits */
-(function(window, $, Smits, undefined) {
+/* global tnt, d3 */
+(function(window, $, tnt, d3, undefined) {
     'use strict';
     console.log('Hello, Tree Viewer app!');
 
     var appContext = $('[data-app-name="tree-viewer-app"]');
-    var defaultTree = '(((Espresso:2,(Milk Foam:2,Espresso Macchiato:5,((Steamed Milk:2,Cappuccino:2,(Whipped Cream:1,Chocolate Syrup:1,Cafe Mocha:3):5):5,Flat White:2):5):5):1,Coffee arabica:0.1,(Columbian:1.5,((Medium Roast:1,Viennese Roast:3,American Roast:5,Instant Coffee:9):2,Heavy Roast:0.1,French Roast:0.2,European Roast:1):5,Brazilian:0.1):1):1,Americano:10,Water:1);';
-    Smits.PhyloCanvas.Render.Style.line.stroke = 'rgb(0,0,255)';    // Color lines blue
-    Smits.PhyloCanvas.Render.Style.text['font-size'] = 10;   // decrease font size
+    var defaultTree = '(((C.elegans,Fly),(((((((((Tasmanian Devil,Wallaby,Opossum),((Armadillo,Sloth),(Rock hyrax,Tenrec,Elephant),(((Rabbit,Pika),(((Rat,Mouse),Kangaroo rat,Squirrel),Guinea Pig)),((Mouse lemur,Bushbaby),((((((Chimp,Human,Gorilla),Orangutan),Gibbon),Macaque),Marmoset),Tarsier)),Tree Shrew),((Microbat,Flying fox),(Hedgehog,Shrew),((Panda,Dog,Domestic ferret),Cat),((Cow,Sheep),Pig,Alpaca,Dolphin),Horse))),Platypus),((((Collared flycatcher,Zebra finch),(Chicken,Turkey),Duck),Chinese softshell turtle),Anole lizard)),Xenopus),Coelacanth),(((Zebrafish,Cave fish),((((Medaka,Platyfish),Stickleback),(Fugu,Tetraodon),Tilapia),Cod)),Spotted gar)),Lamprey),(C.savignyi,C.intestinalis))),S.cerevisiae);';
 
-    /* Generate Tree SVG */
+
+    /* Generate Tree */
     window.addEventListener('Agave::ready', function() {
         console.log('Agave ready...');
-        var render_tree = function render_tree(tree_object,tree_type) {
-            var phylocanvas;
 
-            if (tree_type === 'circular') {
-                phylocanvas = new Smits.PhyloCanvas(
-                    tree_object,
-                    'svgCanvas',
-                    1500, 1500,
-                    'circular'
-                );
-            } else {
-                phylocanvas = new Smits.PhyloCanvas(
-                    tree_object,
-                    'svgCanvas',
-                    1000, 2000
-                );
-            }
+        var render_tree = function render_tree(tree_object) {
+
+            // DOM element
+            var div = $('#tree-canvas', appContext)[0];
+            // tree variables
+            var width = 1000;
+            var scale = false;
+            var transition_speed = 2000;
+            var node_size = 5;
+            var node_stroke = 'black';
+            var node_fill = 'grey';
+            var label_fontsize = 12;
+            var label_height = 20;
+
+            // setup tree
+            var tree = tnt.tree()
+                .node_display(tnt.tree.node_display.circle()
+                    .size(node_size)
+                    .stroke(node_stroke)
+                    .fill(node_fill))
+                .label(tnt.tree.label.text()
+                    .fontsize(label_fontsize)
+                    .height(label_height)
+                    .text(function(node) {
+                        if (node.is_leaf()) {
+                            return node.node_name();
+                        }
+                        return '';
+                    }))
+                .data(tnt.tree.parse_newick(tree_object))
+                .layout(tnt.tree.layout.vertical()
+                    .width(width)
+                    .scale(scale))
+                .duration(transition_speed);
+
+            // set up a layout menu
+            var menu_pane = d3.select(div).append('div').append('span').text('Layout: ');
+            var sel = menu_pane.append('select')
+                               .on('change', function() {
+                                   var layout = tnt.tree.layout[this.value]().width(width).scale(scale);
+                                   tree.layout(layout);
+                                   tree.update();
+                               });
+
+            sel.append('option')
+               .attr('value', 'vertical')
+               .attr('selected', 1)
+               .text('vertical');
+
+            sel.append('option')
+               .attr('value', 'radial')
+               .text('radial');
+
+            tree(div);
         };
 
         $('#file', appContext).on('change', function(evt) {
@@ -50,7 +87,7 @@
 
         $('#clearButton', appContext).on('click', function () {
             // clear the canvas
-            $('#svgCanvas', appContext).empty();
+            $('#tree-canvas', appContext).empty();
 
             $('#file', appContext).val('');
             $('#tree_type', appContext).val('linear');
@@ -59,12 +96,10 @@
 
         $('#tree-render', appContext).submit(function(e) {
             // clear the canvas
-            $('#svgCanvas', appContext).empty();
+            $('#tree-canvas', appContext).empty();
             e.preventDefault();
-            var dataObject = { newick: this.tree.value };
-            console.log(dataObject);
-            render_tree(dataObject,this.tree_type.value);
+            render_tree(this.tree.value);
         });
     });
 
-})(window, jQuery, Smits);
+})(window, jQuery, tnt, d3);
